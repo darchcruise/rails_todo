b32b66da10d1f6a63c0675db3f1e930cb084f570
darchcruise <darchcruise@yahoo.com>
2013-07-09 12:55:56 -0400

    first commit

---
+++ todo/app/controllers/todos_controller.rb
@@ -0,0 +1,21 @@
+class TodoController < ApplicationController
+
+  def list
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+end
+
+
+
+
+
+end
---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '163ef6741d04fb6714e3c5a8175757194443b401e3bce06a31f9b9e83e67b2e3a663bad5938a473303eeed7497c3986afcf3786d1b2825ff164d253454514d3a'
